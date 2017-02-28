4c03100bfa8982f601929c25a0490f3d3ba7aaac
Sandeep L Hegde <hsandeep@vmware.com>
2017-02-28 10:22:21 +0530

    C3 library related changes

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnChanges,ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-barchart',
@@ -7,11 +8,11 @@ import * as d3 from 'd3';
   styleUrls: ['./barchart.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class BarchartComponent implements OnInit, OnChanges {
+export class BarchartComponent implements OnInit, OnChanges{
   @ViewChild('chart') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.createChart();
@@ -22,31 +23,31 @@ export class BarchartComponent implements OnInit, OnChanges {
   }
 
   createChart() {
-    var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-        columns: [
+        var chart = c3.generate({
+          bindto: '#chart',
+          data: {
+          columns: [
             ['data1', 30, 200, 100, 400, 150, 250],
             ['data2', 130, 100, 140, 200, 150, 50]
-        ],
-        type: 'bar'
-    },
-    bar: {
-        width: {
-            ratio: 0.5 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
-    }
-});
+          ],
+          type: 'bar'
+        },
+        bar: {
+          width: {
+          ratio: 0.5 // this makes bar width 50% of length between ticks
+          }
+          // or
+          //width: 100 // this makes bar width 100px
+          }
+        });
 
-setTimeout(function () {
-    chart.load({
-        columns: [
-            ['data3', 130, -150, 200, 300, -200, 100]
-        ]
-    });
-}, 1000);
+        setTimeout(function () {
+          chart.load({
+            columns: [
+             ['data3', 130, -150, 200, 300, -200, 100]
+            ]
+          });
+        });
   }
 
 }
--- src/app/realtime/realtime.component.ts
+++ src/app/realtime/realtime.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
-import * as c3 from 'c3';
+import { Component, OnInit, OnChanges,ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-speedo',
@@ -8,51 +8,51 @@ import * as d3 from 'd3';
   styleUrls: ['./realtime.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class RealtimeComponent implements OnInit, OnChanges {
+export class RealtimeComponent implements OnInit, OnChanges{
   @ViewChild('speedometer') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.createSpeedo();
   }
 
   ngOnChanges() {
-      this.createSpeedo();
+    this.createSpeedo();
   }
 
   createSpeedo() {
-    var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
-            ]
-        }
-    });
-
-    setTimeout(function () {
-        chart.load({
-            columns: [
-                ['data1', 230, 190, 300, 500, 300, 400]
-            ]
+        var chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
         });
-    }, 1000);
 
-    setTimeout(function () {
-        chart.load({
-            columns: [
-                ['data3', 130, 150, 200, 300, 200, 100]
-            ]
-        });
-    }, 1500);
+        setTimeout(function () {
+            chart.load({
+                columns: [
+                    ['data1', 230, 190, 300, 500, 300, 400]
+                ]
+            });
+        }, 1000);
 
-    setTimeout(function () {
-        chart.unload({
-            ids: 'data1'
-        });
-    }, 2000);
+        setTimeout(function () {
+            chart.load({
+                columns: [
+                    ['data3', 130, 150, 200, 300, 200, 100]
+                ]
+            });
+        }, 1500);
+
+        setTimeout(function () {
+            chart.unload({
+                ids: 'data1'
+            });
+        }, 2000);
   }
 }
