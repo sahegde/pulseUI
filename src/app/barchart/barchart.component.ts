2f01a60f1604f63867149e998b346687d86abc14
Sandeep L Hegde <hsandeep@vmware.com>
2017-02-28 20:55:22 +0530

    Adding some more charts

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -16,15 +16,21 @@ export class BarchartComponent implements OnInit, OnChanges{
 
   ngOnInit() {
     this.createChart();
+    this.createSpeedo();
+    this.createSpeedo1();
+    this.createSpeedo2();
   }
 
   ngOnChanges() {
     this.createChart();
+    this.createSpeedo();
+    this.createSpeedo1();
+    this.createSpeedo2();
   }
 
   createChart() {
         var chart = c3.generate({
-          bindto: '#chart',
+          bindto: '#chart3',
           data: {
           columns: [
             ['data1', 30, 200, 100, 400, 150, 250],
@@ -49,5 +55,133 @@ export class BarchartComponent implements OnInit, OnChanges{
           });
         });
   }
+
+    createSpeedo() {
+        var chart = c3.generate({
+            bindto: '#chart2',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
+        });
+
+        setTimeout(function () {
+            chart.load({
+                columns: [
+                    ['data1', 230, 190, 300, 500, 300, 400]
+                ]
+            });
+        }, 1000);
+
+        setTimeout(function () {
+            chart.load({
+                columns: [
+                    ['data3', 130, 150, 200, 300, 200, 100]
+                ]
+            });
+        }, 1500);
+
+        setTimeout(function () {
+            chart.unload({
+                ids: 'data1'
+            });
+        }, 2000);
+  }
+
+      createSpeedo1() {
+            var chart = c3.generate({
+            bindto : "#chart1",
+    data: {
+        columns: [
+            ['data1', 30],
+            ['data2', 120],
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    donut: {
+        title: "Iris Petal Width"
+    }
+});
+
+setTimeout(function () {
+    chart.load({
+        columns: [
+            ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+            ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+            ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+        ]
+    });
+}, 1500);
+
+setTimeout(function () {
+    chart.unload({
+        ids: 'data1'
+    });
+    chart.unload({
+        ids: 'data2'
+    });
+}, 2500);
+      }
+
+      createSpeedo2() {
+        var chart = c3.generate({
+         bindto: '#chart4',
+    data: {
+        columns: [
+            ['data', 91.4]
+        ],
+        type: 'gauge',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    gauge: {
+
+    },
+    color: {
+        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+        threshold: {
+            values: [30, 60, 90, 100]
+        }
+    }
+});
+
+setTimeout(function () {
+    chart.load({
+        columns: [['data', 10]]
+    });
+}, 1000);
+
+setTimeout(function () {
+    chart.load({
+        columns: [['data', 50]]
+    });
+}, 2000);
+
+setTimeout(function () {
+    chart.load({
+        columns: [['data', 70]]
+    });
+}, 3000);
+
+setTimeout(function () {
+    chart.load({
+        columns: [['data', 0]]
+    });
+}, 4000);
+
+setTimeout(function () {
+    chart.load({
+        columns: [['data', 100]]
+    });
+}, 5000);
+
+
+      }
 
 }
